186a38c81445cfd58e68e1ebf6ab73610f9fda28
Sahil Rana <sahilrana27582@gmail.com>
2026-09-23 14:14:13 +0530

    AI SUPPORT | main.py

---
+++ ai-support-assistant/app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+
+from app.routes.routes import router
+from app.config.settings import settings
+from app.core.logging import configure_logging
+from app.exceptions.handlers import app_exception_handler
+from app.exceptions.base import AppException
+
+configure_logging()
+
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.app_version,
+)
+
+
+app.add_exception_handler(
+    AppException,
+    app_exception_handler,
+)
+
+
+@app.get("/health")
+async def health_check():
+
+    return {
+        "status": "UP",
+        "service": settings.app_name,
+        "version": settings.app_version,
+    }
+
+
+app.include_router(router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("app.main:app", port=settings.app_port, reload=True)
